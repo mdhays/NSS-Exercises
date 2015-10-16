9ffc9019779a2f4f27397a129929dee895aff762
mdhays <mdhays1995@gmail.com>
2015-10-16 09:34:51 -0500

    fixed js code to output correctly

--- coins/coins.js
+++ coins/coins.js
@@ -8,12 +8,15 @@ Input: 0.67 Output: { quarters: 2, dimes: 1, nickels: 1, pennies: 2 }
 
 function coinCounter (dollarValue) { // Initialize a JavaScript object to hold the coins var coinPurse = {};
 var coinValues = { quarters: 4, dimes: 10, nickels: 20, pennies: 100 }
-
 var coinPurse = {}
-coinPurse.quarters = dollarValue*4;
-coinPurse.dimes = dollarValue*10;
-coinPurse.nickels = dollarValue*20;
-coinPurse.pennies = dollarValue*100; 
+var cents = dollarValue * 100;
+coinPurse.quarters = Math.floor(cents / 25);
+cent= cents % 25;
+coinPurse.dimes = Math.floor(cents / 10);
+cents = cents % 10;
+coinPurse.nickels = Math.floor(cents / 5)
+cents = cents % 5;
+coinPurse.pennies = Math.floor(cents); 
 return coinPurse; }
 
-console.log(coinCounter(100));
+console.log(coinCounter(50.24));
